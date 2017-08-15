9c06962026dce0bc0b2812fe0796e6fd71a4ebea
Pal Sivertsen <palsivertsen@gmail.com>
2017-08-15 23:05:11 +0200

    Added test for matcher on string validator

--- validators/string_test.go
+++ validators/string_test.go
@@ -9,6 +9,12 @@ import (
 var (
 	validHex   = []string{"abcd", "ABCD", "1234", "abcdefABCDEF1234567890"}
 	invalidHex = []string{" abc", "abcdefg", "", " ", "abc"}
+	validRegex = map[string][]string{
+		"^[a-z]*$": []string{"asd", "abc"},
+	}
+	invalidRegex = map[string][]string{
+		"^[a-c]*$": []string{"abcd", "ABC"},
+	}
 )
 
 func TestHex(t *testing.T) {
@@ -19,5 +25,20 @@ func TestHex(t *testing.T) {
 	for _, invalid := range invalidHex {
 		assert.False(t, String(invalid).IsHex(), invalid)
 		assert.Equal(t, ErrNotHex, String(invalid).Hex().FirstError(), invalid)
+	}
+}
+
+func TestRegex(t *testing.T) {
+	for pattern, values := range validRegex {
+		for _, value := range values {
+			assert.True(t, String(value).IsMatch(pattern), value)
+			assert.Nil(t, String(value).Match(pattern).FirstError(), value)
+		}
+	}
+	for pattern, values := range invalidRegex {
+		for _, value := range values {
+			assert.False(t, String(value).IsMatch(pattern), value)
+			assert.Equal(t, ErrNoMatch, String(value).Match(pattern).FirstError(), value)
+		}
 	}
 }
