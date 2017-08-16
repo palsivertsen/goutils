693a320cb3d671eae3fcf14ef8a3bccd40917553
Pal Sivertsen <palsivertsen@gmail.com>
2017-08-16 14:49:32 +0200

    Added date support for int converter

--- converters/ints.go
+++ converters/ints.go
@@ -1,6 +1,9 @@
 package converters
 
-import "strconv"
+import (
+	"strconv"
+	"time"
+)
 
 type IntConverter struct {
 	i int64
@@ -8,6 +11,14 @@ type IntConverter struct {
 
 func Int(i int) *IntConverter {
 	return &IntConverter{i: int64(i)}
+}
+
+func (c *IntConverter) ToTimeFromNsec() time.Time {
+	return time.Unix(0, c.i)
+}
+
+func (c *IntConverter) ToTimeFromSec() time.Time {
+	return time.Unix(c.i, 0)
 }
 
 func (c *IntConverter) ToString() string {
--- converters/ints_test.go
+++ converters/ints_test.go
@@ -2,10 +2,16 @@ package converters
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInt(t *testing.T) {
+func TestString(t *testing.T) {
 	assert.Equal(t, "2147483647", Int(2147483647).ToString())
+}
+
+func TestDate(t *testing.T) {
+	assert.Equal(t, time.Unix(0, 1502883134757394503), Int(1502883134757394503).ToTimeFromNsec())
+	assert.Equal(t, time.Unix(1502883134, 0), Int(1502883134).ToTimeFromSec())
 }
