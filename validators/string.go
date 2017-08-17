e0aedd8f023afc49118639b3839f64e9c5d2ac0b
Pal Sivertsen <palsivertsen@gmail.com>
2017-08-17 12:22:27 +0200

    Added length validators to string validator

--- validators/string.go
+++ validators/string.go
@@ -13,6 +13,12 @@ var (
 	ErrNoMatch = errors.New("String no match")
 	// ErrFunc indicates an error when validating using func
 	ErrFunc = errors.New("func did not validate to true")
+	// ErrMax validator length too long
+	ErrMax = errors.New("String is too long")
+	// ErrMin validator length too short
+	ErrMin = errors.New("String is too short")
+	// ErrLen invalid validator length
+	ErrLen = errors.New("String is not correct length")
 )
 
 // StringValidator is a validator used to validate strings
@@ -65,10 +71,13 @@ func (v *StringValidator) Match(pattern string) *StringValidator {
 	return v
 }
 
+// IsFunc executes the given func with the validator value as parameter and returns the result
 func (v *StringValidator) IsFunc(f func(string) bool) bool {
 	return f(v.value)
 }
 
+// Func registers an error with the validator if IsFunc returns false
+// Is chainable
 func (v *StringValidator) Func(f func(string) bool) *StringValidator {
 	if !v.IsFunc(f) {
 		v.Error(ErrFunc)
@@ -76,15 +85,44 @@ func (v *StringValidator) Func(f func(string) bool) *StringValidator {
 	return v
 }
 
-//
-// func (v *StringValidator) IsMaxLen(len int) *StringValidator {
-//
-// }
-//
-// func (v *StringValidator) IsMinLen(len int) *StringValidator {
-//
-// }
-//
-// func (v *StringValidator) IsMaxLen(len int) *StringValidator {
-//
-// }
+// IsMaxLen checks if the validator has a maximun length of l
+func (v *StringValidator) IsMaxLen(l int) bool {
+	return len(v.value) <= l
+}
+
+// MaxLen registers an error with the validator if IsMaxLen returns false
+// Is chainable
+func (v *StringValidator) MaxLen(l int) *StringValidator {
+	if !v.IsMaxLen(l) {
+		v.Error(ErrMax)
+	}
+	return v
+}
+
+// IsMinLen checks if the validator has a minimum length of l
+func (v *StringValidator) IsMinLen(l int) bool {
+	return len(v.value) >= l
+}
+
+// MinLen registers an error with the validator if IsMinLen returns false
+// Is chainable
+func (v *StringValidator) MinLen(l int) *StringValidator {
+	if !v.IsMinLen(l) {
+		v.Error(ErrMin)
+	}
+	return v
+}
+
+// IsLen checks if the validator has a length of l
+func (v *StringValidator) IsLen(l int) bool {
+	return len(v.value) == l
+}
+
+// Len registers an error with the validator if IsLen returns false
+// Is chainable
+func (v *StringValidator) Len(l int) *StringValidator {
+	if !v.IsLen(l) {
+		v.Error(ErrLen)
+	}
+	return v
+}
--- validators/string_test.go
+++ validators/string_test.go
@@ -27,6 +27,32 @@ var (
 			func(value string) bool { return value != "some string" },
 		},
 	}
+	validMaxLen = map[int][]string{
+		1: []string{"a", "b", "c"},
+		2: []string{"a", "bb"},
+		3: []string{"a", "bb", "ccc"},
+	}
+	invalidMaxLen = map[int][]string{
+		1: []string{"aa", "bbb", "cccc"},
+		2: []string{"aaa", "bbbb", "ccccc"},
+	}
+	validMinLen = map[int][]string{
+		1: []string{"a", "bb", "ccc"},
+		2: []string{"aa", "bbb", "cccc"},
+	}
+	invalidMinLen = map[int][]string{
+		1: []string{""},
+		2: []string{"a", "b", "c"},
+		5: []string{"aa", "bbb", "cccc"},
+	}
+	validLen = map[int][]string{
+		1: []string{"a", "b", "c"},
+		2: []string{"aa", "bb", "cc"},
+	}
+	invalidLen = map[int][]string{
+		1: []string{"aa", "bb", "cc"},
+		2: []string{"aaa", "bbb", "ccc"},
+	}
 )
 
 func TestHex(t *testing.T) {
@@ -66,6 +92,51 @@ func TestFunc(t *testing.T) {
 		for _, f := range funcs {
 			assert.False(t, String(value).IsFunc(f), value)
 			assert.Equal(t, ErrFunc, String(value).Func(f).FirstError(), value)
+		}
+	}
+}
+
+func TestMaxLen(t *testing.T) {
+	for l, values := range validMaxLen {
+		for _, value := range values {
+			assert.True(t, String(value).IsMaxLen(l), "%s !max %d", value, l)
+			assert.Nil(t, String(value).MaxLen(l).FirstError(), "%s !max %d", value, l)
+		}
+	}
+	for l, values := range invalidMaxLen {
+		for _, value := range values {
+			assert.False(t, String(value).IsMaxLen(l), "%s !max %d", value, l)
+			assert.Equal(t, ErrMax, String(value).MaxLen(l).FirstError(), "%s !max %d", value, l)
+		}
+	}
+}
+
+func TestMinLen(t *testing.T) {
+	for l, values := range validMinLen {
+		for _, value := range values {
+			assert.True(t, String(value).IsMinLen(l), "%s !min %d", value, l)
+			assert.Nil(t, String(value).MinLen(l).FirstError(), "%s !min %d", value, l)
+		}
+	}
+	for l, values := range invalidMinLen {
+		for _, value := range values {
+			assert.False(t, String(value).IsMinLen(l), "%s !min %d", value, l)
+			assert.Equal(t, ErrMin, String(value).MinLen(l).FirstError(), "%s !min %d", value, l)
+		}
+	}
+}
+
+func TestLen(t *testing.T) {
+	for l, values := range validLen {
+		for _, value := range values {
+			assert.True(t, String(value).IsLen(l), "%s !len %d", value, l)
+			assert.Nil(t, String(value).Len(l).FirstError(), "%s !len %d", value, l)
+		}
+	}
+	for l, values := range invalidLen {
+		for _, value := range values {
+			assert.False(t, String(value).IsLen(l), "%s !len %d", value, l)
+			assert.Equal(t, ErrLen, String(value).Len(l).FirstError(), "%s !len %d", value, l)
 		}
 	}
 }
