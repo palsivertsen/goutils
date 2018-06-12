e3046bde2215fc1b8873f149d57aed1de7d7ac11
Pal Sivertsen <palsivertsen@gmail.com>
2018-06-12 15:17:18 +0200

    Removed links to sub packages as they are listed implicitly

--- goutils.go
+++ goutils.go
@@ -1,6 +1,2 @@
 // Package goutils provides common utilities
-//
-// See also:
-// https://godoc.org/github.com/palsivertsen/goutils/converters
-// https://godoc.org/github.com/palsivertsen/goutils/validators
 package goutils
