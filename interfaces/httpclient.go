12451b08903d9bdad3f5e654563ce0462e09a1f6
Pal Sivertsen <palsivertsen@gmail.com>
2018-06-13 10:04:48 +0200

    Added interfaces package

---
+++ interfaces/httpclient.go
@@ -0,0 +1,16 @@
+package interfaces
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// HTTPClient interface for http.Client
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+	Get(url string) (resp *http.Response, err error)
+	Head(url string) (resp *http.Response, err error)
+	Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
+	PostForm(url string, data url.Values) (resp *http.Response, err error)
+}
---
+++ interfaces/interfaces.go
@@ -0,0 +1,2 @@
+// Package interfaces provides interfaces to easily create mocks for common types, like http.Client, during testing
+package interfaces
